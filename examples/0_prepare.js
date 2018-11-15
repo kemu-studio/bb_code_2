ae9aab7826445a9e586ddc110855fa6da7ce1c5c
Sylwester Wysocki <dzik@ke.mu>
2018-11-15 16:22:11 +0100

    Examples: Added table of contents to gallery html.

--- examples/0_prepare.js
+++ examples/0_prepare.js
@@ -1,16 +1,15 @@
-//
-// This script prepares node_modules/bb2 directory to emulate bb2 package
-// installed by:
-// $npm install bb2
-// command, but using *LOCAL* files.
-//
-
 const fs   = require('fs-extra')
 const glob = require('glob')
 const path = require('path')
 const exec = require('child_process').execSync
 
 const GALLERY_HTML_FILE = './gallery.html'
+
+//
+// Prepare node_modules/bb2 directory to emulate bb2 package installed by:
+// $npm install bb2
+// command, but using *LOCAL* files.
+//
 
 // Prepare clean, empty node_modules directory.
 console.log('Preparing node_modules directory...')
@@ -29,21 +28,40 @@ glob.sync('../src/*').forEach((item) => {
 console.log('Copying [ package.json ]...')
 fs.copySync('../package.json', './node_modules/bb2/package.json')
 
-// Prepare gallery HTML file with all examples rendered.
-fs.writeFileSync(GALLERY_HTML_FILE, '')
+//
+// Prepare gallery HTML file with preview of all avaialble examples.
+//
 
-glob.sync('./example_*.js').forEach((item) => {
+const exampleFiles = glob.sync('./example_*.js')
+
+// Table of content.
+let examplesIndex = '<b>Table of contents</b>:<ul>';
+
+exampleFiles.forEach((item) => {
+  const fname = path.basename(item)
+  examplesIndex += '<li><a href="#' + fname + '">' + fname + '</li>'
+})
+
+examplesIndex += '</ul>'
+fs.writeFileSync(GALLERY_HTML_FILE, examplesIndex)
+
+// Examples preview.
+exampleFiles.forEach((item) => {
   const fname = path.basename(item)
   console.log('Rendering [', fname, ']...')
 
   // Added header before each file.
-  fs.appendFileSync(GALLERY_HTML_FILE, '<hr><a href="' + item + '">' + fname + '</a>:<br><br>')
+  fs.appendFileSync(GALLERY_HTML_FILE,
+    '<hr><a id="' + fname + '" href="' + item + '">' + fname + '</a> (click to see source code):<br><br>')
 
   // Render script to file.
   exec('node ' + item + ' >> ' + GALLERY_HTML_FILE)
 })
 
+//
 // Success!
+//
+
 console.log('')
 console.log('Done! Now You can *RUN* one of \'example_xxx.js\' script and see BB Code 2.0 in action!')
 console.log('Alternatively, you can open the \'gallery.html\' file in your browser and *SEE* what BB Code 2.0 can do for you.')
