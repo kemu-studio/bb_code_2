2ffe8c691b6c0cd993dac909a8bf671a0c92f273
Sylwester Wysocki <dzik@ke.mu>
2018-11-15 14:01:16 +0100

    Examples: Added helper script to emulate installed bb2 packages using local files.

---
+++ examples/0_prepare.js
@@ -0,0 +1,30 @@
+//
+// This script prepares node_modules/bb2 directory to emulate bb2 package
+// installed by:
+// $npm install bb2
+// command, but using *LOCAL* files.
+//
+
+const fs   = require('fs-extra')
+const glob = require('glob')
+const path = require('path')
+
+// Prepare clean, empty node_modules directory.
+console.log('Preparing node_modules directory...')
+fs.removeSync('./node_modules')
+fs.mkdirpSync('./node_modules')
+fs.mkdirpSync('./node_modules/bb2')
+
+// Copy js files to node_modules/bb2.
+glob.sync('../src/*').forEach((item) => {
+  const fname = path.basename(item)
+  console.log('Copying [', fname, ']...')
+  fs.copySync(item, './node_modules/bb2/' + fname)
+})
+
+// Copy package.json from root.
+console.log('Copying [ package.json ]...')
+fs.copySync('../package.json', './node_modules/bb2/package.json')
+
+// Success!
+console.log('Done! Now You can run one of \'example_xxx.js\' script and see KBB2 in action!')
