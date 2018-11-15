9f4e062bf2234176ecde194122d163d296088b29
Sylwester Wysocki <dzik@ke.mu>
2018-11-15 15:09:21 +0100

    Examples: Renamed example_0 to example_basic.

--- examples/example_basic.js
+++ examples/example_basic.js
@@ -1,4 +1,4 @@
-// example_01.js
+// example_basic.js
 const BBCodeHtml = require('bb2').BBCodeHtml
 const bb         = new BBCodeHtml()
 console.log(bb.parse('visible [0]invisible[1] [b]bold[/] [i]italic[/] [u]underline[/]'))
