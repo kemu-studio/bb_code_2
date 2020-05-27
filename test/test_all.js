64f34323db554e68ca545719c05a70435427da2b
Roman Pietrzak <y@ke.mu>
2020-05-27 13:17:11 +0100

    Fixed paths concatenation issue

--- test/test_all.js
+++ test/test_all.js
@@ -1,4 +1,4 @@
 const Mocha = require('mocha')
 const mocha = new Mocha({ui: 'tdd', reporter: 'spec', bail: 'yes'})
-mocha.addFile(__dirname + '\\test_bbCode.js')
+mocha.addFile(__dirname + '/test_bbCode.js')
 mocha.run()
